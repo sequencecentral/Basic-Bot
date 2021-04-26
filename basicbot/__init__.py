a43a31db6f34c771a84f5fd999b38ba2a308f14c
Steve Ayers <californiastephen@outlook.com>
2021-04-26 10:36:22 -0700

    v1.4.1

--- basicbot/__init__.py
+++ basicbot/__init__.py
@@ -1,9 +1,18 @@
-import nltk
 import subprocess
 from .chat import *
 from .sentiment import *
 from .responder import *
 from .tagger import *
+
+import nltk
+try:
+    print("Downloading nltk data")
+    nltk.download('wordnet')
+    nltk.download('pros_cons')
+    nltk.download('punkt')
+    nltk.download('averaged_perceptron_tagger')
+except Exception as e:
+    print(e)
 
 # print("Current directory: "+ strEXIT()
 # t("ls")))
